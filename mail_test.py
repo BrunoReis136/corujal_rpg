84ad618d4c5e5b33ac800105f1f2b9b89917a28a
Bruno Henrique Reis <bruhreis136@hotmail.com>
2025-09-25 22:06:58 -0300

    Update mail_test.py

--- mail_test.py
+++ mail_test.py
@@ -1,7 +1,7 @@
 import smtplib
 
 user = "brunohreis136@gmail.com"
-app_password = "ghnjadegvmvslyst"
+app_password = "llbiromlfalzgqkp"
 
 try:
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
